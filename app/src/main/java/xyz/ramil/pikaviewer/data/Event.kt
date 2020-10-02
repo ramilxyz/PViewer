0d4cda2e641aee08f1627a80d904a6c99fdba739
ramilabdrakhmanov <ramilabdrakhmanov17@gmail.com>
2020-10-02 20:44:05 +0500

    add viewpager

--- app/src/main/java/xyz/ramil/pikaviewer/data/Event.kt
+++ app/src/main/java/xyz/ramil/pikaviewer/data/Event.kt
@@ -6,11 +6,9 @@ data class Event<out T>(val status: Status, val data: Any?, val error: Error?) {
         fun <T> loading(): Event<T> {
             return Event(Status.LOADING, null, null)
         }
-
         fun <T> success(data: Any?): Event<T> {
             return Event(Status.SUCCESS, data, null)
         }
-
         fun <T> error(error: Error?): Event<T> {
             return Event(Status.ERROR, null, error)
         }
