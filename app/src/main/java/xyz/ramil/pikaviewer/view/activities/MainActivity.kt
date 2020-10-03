2b31d8917fe16e9d9f7111a396ea30afad2fd109
ramilabdrakhmanov <ramilabdrakhmanov17@gmail.com>
2020-10-03 13:25:28 +0500

    add viewpager

--- app/src/main/java/xyz/ramil/pikaviewer/view/activities/MainActivity.kt
+++ app/src/main/java/xyz/ramil/pikaviewer/view/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package xyz.ramil.pikaviewer.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import xyz.ramil.pikaviewer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount > 0) {
+                getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+            } else {
+                getSupportActionBar()?.setDisplayHomeAsUpEnabled(false)
+
+            }
+        }
+
     }
+
 }
